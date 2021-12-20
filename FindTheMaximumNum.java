3e42f75646b3baf5886713677be06eb93182d06a
Achyuth Naidu <ashwath.bly@gamil.com>
2021-12-20 16:37:28 +0530

    UC2 Given 3 Float find the maximum

--- FindTheMaximumNum.java
+++ FindTheMaximumNum.java
@@ -20,12 +20,24 @@ public class FindTheMaximumNum  {
 		System.out.println("The maximum Integer value is : " + max);
 	}
 
-	
+	/*
+	 * UC2 Given 3 Float find the maximum
+	 */
+	public void maxmumFloatNum(Float A, Float B, Float C) {
+		Float max = A;
+		if (B.compareTo(max) > 0) {
+			max = B;
+		}
+		if (C.compareTo(max) > 0) {
+			max = C;
+		}
+		System.out.println("The maximum Float value is : " + max);
+	}
+
 	public static void main(String[] args) {
 		FindTheMaximumNum g = new FindTheMaximumNum();
-		
-		g.maxmumIntegerNum(20, 5, 30);// UC1
-
+//		g.maxmumIntegerNum(20, 5, 30);// UC1
+		g.maxmumFloatNum(50.0f, 20.0f, 30.0f);// UC2
 	}
 
 }
