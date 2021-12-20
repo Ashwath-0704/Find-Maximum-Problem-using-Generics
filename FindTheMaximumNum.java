6c17f7786c31a33e61e66561f3a0e957cc721f26
Achyuth Naidu <ashwath.bly@gamil.com>
2021-12-20 16:43:28 +0530

    UC4 using Generic

--- FindTheMaximumNum.java
+++ FindTheMaximumNum.java
@@ -48,12 +48,57 @@ public class FindTheMaximumNum  {
 		System.out.println("The maximum String value is : " + max);
 
 	}
+
+	/*
+	 * Refactor all the 3 to One Generic Method and find the maximum
+	 * 
+	 */
+	public static <T extends Comparable<T>> T maximum(T A, T B, T C) {
+		T max = A;
+		if (B.compareTo(max) > 0) {
+			max = B;
+		}
+		if (C.compareTo(max) > 0) {
+			max = C;
+		}
+		System.out.println("The maximum String value is : " + max);
+		return max;
+
+	}
+
+	/*
+	 * UC4
+	 * Extend the max method to take more then three parameters
+	 * 
+	 */
+	
+	public static <T extends Comparable<T>> T maximum1(T A,T B,T C,T D) {
+		T max = A;
+		if (B.compareTo(max) > 0) {
+			max = B;
+		}
+		if (C.compareTo(max) > 0) {
+			max = C;
+		}
+		if (D.compareTo(max) > 0) {
+			max = D;
+		}
+		System.out.println("The maximum String value is : " + max);
+		return max;
+	}
 	public static void main(String[] args) {
 		FindTheMaximumNum g = new FindTheMaximumNum();
 		
 //		g.maxmumIntegerNum(20, 5, 30);// UC1
 //		g.maxmumFloatNum(50.0f, 20.0f, 30.0f);// UC2
-		g.maxmumStringNum("pear", "apple", "orange");// UC3
+//		g.maxmumStringNum("pear", "apple", "orange");// UC3
+     
+//UC4
+		g.maximum(20, 5, 30);
+		g.maximum(50.0f, 20.0f, 30.0f);
+		g.maximum("pear", "apple", "orange");
+
+
 	}
 
 }
