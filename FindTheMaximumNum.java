e7b849eca3dde6e0e290c187db11f87cb5bbe371
Achyuth Naidu <ashwath.bly@gamil.com>
2021-12-20 16:39:30 +0530

    UC3 Given 3 String find the maximum

--- FindTheMaximumNum.java
+++ FindTheMaximumNum.java
@@ -34,10 +34,26 @@ public class FindTheMaximumNum  {
 		System.out.println("The maximum Float value is : " + max);
 	}
 
+	/*
+	 * UC3 Given 3 Strings find the maximum
+	 */
+	public void maxmumStringNum(String A, String B, String C) {
+		String max = A;
+		if (B.compareTo(max) > 0) {
+			max = B;
+		}
+		if (C.compareTo(max) > 0) {
+			max = C;
+		}
+		System.out.println("The maximum String value is : " + max);
+
+	}
 	public static void main(String[] args) {
 		FindTheMaximumNum g = new FindTheMaximumNum();
+		
 //		g.maxmumIntegerNum(20, 5, 30);// UC1
-		g.maxmumFloatNum(50.0f, 20.0f, 30.0f);// UC2
+//		g.maxmumFloatNum(50.0f, 20.0f, 30.0f);// UC2
+		g.maxmumStringNum("pear", "apple", "orange");// UC3
 	}
 
 }
